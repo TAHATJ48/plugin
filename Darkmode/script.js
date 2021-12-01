7fa91f3d04d5dfbf0c19788d1d9b9d5b4bb3a1b7
Noémie <noemie.kpatenon@supinternet.fr>
2021-12-01 13:00:07 +0100

    Update script.js

--- Darkmode/script.js
+++ Darkmode/script.js
@@ -5,9 +5,24 @@ document.body.appendChild(btn);
 function Darkmode () {
     var element = document.body;
     element.classList.toggle("dark-mode");
- }
-btn.onclick = () => {
-    alert("bouton cliqué");
-    Darkmode();
-  };
+}
 
+if (localStorage.isDark == "true") {
+    btn.innerHTML = "désactiver le mode sombre";
+    Darkmode();
+}
+
+btn.onclick = () => {
+    if (localStorage.isDark == "false") {
+        btn.innerHTML = "activer le mode sombre";
+        localStorage.setItem("isDark", "true")
+        Darkmode()
+        alert(localStorage.getItem("isDark"))
+    }
+    else{
+        btn.innerHTML = "désactiver le mode sombre";
+        localStorage.setItem("isDark", "false")
+        Darkmode()
+        alert(localStorage.getItem("isDark"))
+    }
+};
